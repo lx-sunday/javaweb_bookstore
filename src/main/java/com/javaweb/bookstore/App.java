df84e6383bf11aba2589ab8ec05809f405be7430
liaoxue <1205773522@qq.com>
2018-06-14 14:12:19 +0800

    startjjjjjjjj

---
+++ src/main/java/com/javaweb/bookstore/App.java
@@ -0,0 +1,13 @@
+package com.javaweb.bookstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
